8f65a59f63b8fffaac9656294ba0a5bbca6eb170
Kay <duddnr654@naver.com>
2023-04-17 20:19:28 +0900

    [FEAT]: Layout 적용

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,26 @@
+import styled from "@emotion/styled";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+  height: 100%;
+  background-color: var(--background-color-secondary);
+`;
+
+const Styled = {
+  Wrapper,
+};
+
+const Layout = ({ children }) => {
+  return (
+    <Styled.Wrapper>
+      <main>{children}</main>
+    </Styled.Wrapper>
+  );
+};
+
+export default Layout;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '~/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import "~/styles/globals.css";
+import { Layout } from "~/components/layout";
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+};
+
+export default App;
